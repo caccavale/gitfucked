92ca3d9dee45a8a1887becffefcb4df5e8690f7d
Sam Caccavale <caccavale@users.noreply.github.com>
2018-04-19 15:10:28 -0400

    optimizing python for some reason

--- starpy.py
+++ starpy.py
@@ -2,8 +2,6 @@ import logging
 import sys
 
 import github
-github.Repository.Repository.__hash__ = lambda self: hash(self.full_name)
-github.Repository.Repository.__eg__ = lambda self, other: self.full_name == other.full_name
 
 logging.basicConfig(level=logging.INFO)
 
@@ -15,21 +13,21 @@ with open('token', 'r') as f:
 
 dst = g.get_user()
 logging.info('Getting stars...')
-stars = set(dst.get_starred())
+stars = {repo.full_name for repo in dst.get_starred()}
 logging.info('...stars got.')
+
+
+# Define the qualifications upon which to filter here.
+def is_interesting(repo):
+    return repo not in stars
+
 
 for user in sys.argv[1:]:
     logging.info('Stealing stars from %s' % user)
     src = g.get_user(user)
 
-    logging.info('    precompute boy...')
-    diff = set(src.get_starred()) - stars
-    logging.info('    ...is done now, diff size is %d.' % len(diff))
-
-    # If you want, filter here. Ex:
-    # diff = set(filter(lambda x: x.name.startswith('py'), diff))
-
-    for repo in diff:
+    # Misuse of a generator to make this a one liner:
+    for repo in filter(is_interesting, src.get_starred()):
         logging.info('      starring %s' % repo.full_name)
         try:
             dst.add_to_starred(repo)
