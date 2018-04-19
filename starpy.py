3fb164f425108fed4a7b5afc346bb77b25acceee
Sam Caccavale <caccavale@users.noreply.github.com>
2018-04-19 14:52:09 -0400

    Update starpy.py

--- starpy.py
+++ starpy.py
@@ -7,27 +7,34 @@ github.Repository.Repository.__eg__ = lambda self, other: self.full_name == othe
 
 logging.basicConfig(level=logging.INFO)
 
-if len(sys.argv) is not 2:
+if len(sys.argv) < 2:
     exit()
 
 with open('token', 'r') as f:
     g = github.Github(f.read().strip('\n'))
 
 dst = g.get_user()
-src = g.get_user(sys.argv[1])
+logging.info('Getting stars...')
+stars = set(dst.get_starred())
+logging.info('...stars got.')
 
-logging.info('Precompute boy...')
-diff = set(src.get_starred()) - set(dst.get_starred())
-logging.info('...is done now, diff size is %d.' % len(diff))
+for user in sys.argv[1:]:
+    logging.info('Stealing stars from %s' % user)
+    src = g.get_user(user)
 
-# If you want, filter here. Ex:
-# diff = set(filter(lambda x: x.name.startswith('py'), diff))
+    logging.info('    precompute boy...')
+    diff = set(src.get_starred()) - stars
+    logging.info('    ...is done now, diff size is %d.' % len(diff))
 
-for repo in diff:
-    logging.info('Starring %s' % repo.full_name)
-    try:
-        dst.add_to_starred(repo)
-    except KeyboardInterrupt:
-        raise
-    except Exception as e:
-        logging.warning('Failed on %s with error: %s' % (repo.full_name, e.data.get('message', e)))
+    # If you want, filter here. Ex:
+    # diff = set(filter(lambda x: x.name.startswith('py'), diff))
+
+    for repo in diff:
+        logging.info('      starring %s' % repo.full_name)
+        try:
+            dst.add_to_starred(repo)
+            stars.add(repo)
+        except KeyboardInterrupt:
+            raise
+        except Exception as e:
+            logging.warning('      ^ failed on %s with error: %s' % (repo.full_name, e.data.get('message', e)))
